4c91b6442e6f67a7fef64a07f1df81802b46487e
Luc Swinkels <luc.swinkels@live.nl>
2024-02-28 10:25:16 +0100

    fix: fix post images centering alt text

--- components/post.tsx
+++ components/post.tsx
@@ -30,9 +30,10 @@ const builder = imageUrlBuilder(client);
 export default function Post({ post }: { post: SanityDocument }) {
   //TODO: Fix typing
   const ImageComponent = ({ value }: any) => {
+    console.log(value);
     const { width, height } = getImageDimensions(value);
     return (
-      <div className="my-16">
+      <div className="my-16 flex justify-center flex-col w-max max-w-full">
         <Image
           src={builder.image(value).width(width).height(height).url()}
           alt={value.alt}
@@ -41,7 +42,7 @@ export default function Post({ post }: { post: SanityDocument }) {
           loading="lazy"
           className="rounded-lg border mb-2"
         />
-        <MutedText className="m-0 italic">{value.alt}</MutedText>
+        <MutedText className="mx-auto mt-0 italic">{value.alt}</MutedText>
       </div>
     );
   };
