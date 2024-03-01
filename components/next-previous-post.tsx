549338eeed2d8cf00e9bab3f912c42d523c216c2
Luc Swinkels <luc.swinkels@live.nl>
2024-03-01 11:45:53 +0100

    a

--- components/next-previous-post.tsx
+++ components/next-previous-post.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
+import { client } from "@/sanity/lib/client";
+import imageUrlBuilder from "@sanity/image-url";
 import { SanityDocument } from "next-sanity";
 
 import { H3 } from "./typography/h3";
 import { H4 } from "./typography/h4";
 import { P } from "./typography/p";
 import { Card } from "./ui/card";
+
+const builder = imageUrlBuilder(client);
 
 export default function NextPreviousPost({
   nextPost,
@@ -24,6 +29,18 @@ export default function NextPreviousPost({
             href={`/${previousPost.slug.current}`}
             className="group lg:basis-1/2 basis-full w-full p-8 hover:bg-accent border-b lg:border-b-0 lg:border-r transition-colors"
           >
+            <Image
+              src={builder
+                .image(previousPost.mainImage)
+                .width(200)
+                .height(200)
+                .url()}
+              className="border shadow-lg rounded-xl mb-8"
+              width={200}
+              height={200}
+              quality={100}
+              alt={previousPost.mainImage.alt}
+            />
             <div className="flex flex-col gap-1 lg:gap-2 transition-transform group-hover:translate-x-2">
               <P className="text-muted-foreground">Previous post</P>
               <H3 className="line-clamp-1">{previousPost.title}</H3>
@@ -33,6 +50,18 @@ export default function NextPreviousPost({
             href={`/${nextPost.slug.current}`}
             className="group lg:basis-1/2 basis-full w-full p-8 hover:bg-accent transition-colors"
           >
+            <Image
+              src={builder
+                .image(nextPost.mainImage)
+                .width(200)
+                .height(200)
+                .url()}
+              className="border shadow-lg rounded-xl mb-8"
+              width={200}
+              height={200}
+              quality={100}
+              alt={nextPost.mainImage.alt}
+            />
             <div className="flex flex-col gap-1 lg:gap-2 transition-transform group-hover:translate-x-2">
               <P className="text-muted-foreground">Next post</P>
               <H3 className="line-clamp-1">{nextPost.title}</H3>
