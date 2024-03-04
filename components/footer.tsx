719c1e58cc886e552d3e8c10da1ce616dc29a735
Luc Swinkels <luc.swinkels@live.nl>
2024-03-04 16:24:02 +0100

    style: dry code improvements

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,6 +8,23 @@ import Container from "./container";
 import { Logo } from "./logo";
 
 export default function Footer() {
+  const linkStyles =
+    "hover:border-foreground border-b transition-colors border-transparent pb-0.5";
+  const links = [
+    {
+      href: "/reading-guide",
+      text: "Reading guide",
+    },
+    {
+      href: "/burden-of-proof",
+      text: "Burden of proof",
+    },
+    {
+      href: "https://github.com/lucswinkels/graduation-portfolio",
+      text: "Source code",
+      external: true,
+    },
+  ];
   return (
     <>
       <div className="w-full py-16 lg:py-24 xl:py-32 bg-accent border-t relative overflow-hidden">
@@ -16,30 +33,19 @@ export default function Footer() {
           <div className="flex flex-col lg:flex-row justify-between lg:items-center gap-8">
             <Logo className="size-12 lg:size-14 xl:size-18" />
             <ul className="flex flex-col lg:flex-row gap-4 lg:gap-8 lg:items-center w-max">
-              <li>
-                <Link
-                  href="/reading-guide"
-                  className="hover:border-foreground border-b transition-colors border-transparent pb-0.5"
-                >
-                  Reading guide
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/burden-of-proof"
-                  className="hover:border-foreground border-b transition-colors border-transparent pb-0.5"
-                >
-                  Burden of proof
-                </Link>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/lucswinkels/graduation-portfolio"
-                  className="flex items-center hover:border-foreground border-b transition-colors border-transparent pb-0.5"
-                >
-                  Source code <ExternalLink className="size-4 ml-2" />
-                </a>
-              </li>
+              {links.map((link) => (
+                <li key={link.href}>
+                  {link.external ? (
+                    <a href={link.href} className={linkStyles}>
+                      {link.text}
+                    </a>
+                  ) : (
+                    <Link href={link.href} className={linkStyles}>
+                      {link.text}
+                    </Link>
+                  )}
+                </li>
+              ))}
             </ul>
           </div>
         </Container>
