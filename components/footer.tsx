f9c2b9bc1a7609b4583b26aa1e983efc315e1d8e
Luc Swinkels <luc.swinkels@live.nl>
2024-03-05 09:29:55 +0100

    refactor: update footer styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -30,9 +30,9 @@ export default function Footer() {
       <div className="w-full py-16 lg:py-24 xl:py-32 bg-accent-subtle border-t relative overflow-hidden">
         {/* <CardPattern className="opacity-5" /> */}
         <Container className="relative z-20">
-          <div className="flex flex-col lg:flex-row justify-between lg:items-center gap-8">
+          <div className="flex flex-col md:flex-row justify-between md:items-center gap-8">
             <Logo className="size-12 lg:size-14 xl:size-18" />
-            <ul className="flex flex-col lg:flex-row gap-4 lg:gap-8 lg:items-center w-max">
+            <ul className="flex flex-col md:flex-row gap-4 md:gap-8 md:items-center w-max">
               {links.map((link) => (
                 <li key={link.href}>
                   {link.external ? (
@@ -50,10 +50,13 @@ export default function Footer() {
           </div>
         </Container>
       </div>
-      <div className="bg-background w-full border-t py-8">
-        <Container>
+      <div className="bg-accent-subtle w-full border-t py-8">
+        <Container className="flex justify-between flex-col md:flex-row ">
           <span className="text-sm text-muted-foreground">
             &copy; {new Date().getFullYear()} Luc Swinkels.
+          </span>
+          <span className="text-sm text-muted-foreground">
+            All rights reserved.
           </span>
         </Container>
       </div>
