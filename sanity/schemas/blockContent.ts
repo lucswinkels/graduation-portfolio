ed82b1e0a1f699695f9a2e58bf4746f9aa7a5634
Luc Swinkels <luc.swinkels@live.nl>
2024-03-06 14:21:27 +0100

    refactor: change sanity image/video schema alt to string instead of textbox

--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -68,7 +68,7 @@ export default defineType({
       fields: [
         {
           name: "alt",
-          type: "text",
+          type: "string",
           title: "Alternative text",
           validation: (Rule) =>
             Rule.error("You have to fill out the alternative text.").required(),
@@ -82,7 +82,7 @@ export default defineType({
       fields: [
         {
           name: "alt",
-          type: "text",
+          type: "string",
           title: "Alternative text",
           validation: (Rule) =>
             Rule.error("You have to fill out the alternative text.").required(),
