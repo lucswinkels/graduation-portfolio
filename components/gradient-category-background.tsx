1aa4116bb04cd71f0bc4ccdd17b512f41002628a
Luc Swinkels <luc.swinkels@live.nl>
2024-04-11 15:38:56 +0200

    fix: fix tailwind not understanding gradient values in jit from lib file

--- components/gradient-category-background.tsx
+++ components/gradient-category-background.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { CATEGORY_GRADIENT_VARIANTS } from "@/lib/constants";
 import { cn } from "@/lib/utils";
+
+const CATEGORY_GRADIENT_VARIANTS = {
+  research: "from-green-500 to-teal-500",
+  design: "from-violet-500 to-fuchsia-500",
+  development: "from-rose-500 to-red-500",
+  docs: "from-yellow-500 to-orange-500",
+  testing: "from-cyan-500 to-sky-500",
+  other: "from-slate-500 to-slate-800",
+};
 
 export function GradientCategoryBackground({
   children,
