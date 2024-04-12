0f6362d10891c088af4c636fc5822d1decc6f3dd
Luc Swinkels <luc.swinkels@live.nl>
2024-04-12 09:31:42 +0200

    fix: fix mobile menu landscape spacing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -115,7 +115,7 @@ export function Navbar() {
     // ];
     return (
       <div
-        className={`lg:hidden p-6 md:p-[10%] font-medium flex-col fixed top-16 left-0 z-40 w-full bg-background h-[calc(100dvh-4rem)] landscape:min-h-[calc(100dvh-4rem)] landscape:h-auto ${
+        className={`lg:hidden p-6 md:p-[10%] landscape:md:py-6 font-medium flex-col fixed top-16 left-0 z-40 w-full bg-background h-[calc(100dvh-4rem)] ${
           mobileMenuVisibility ? "flex" : "hidden"
         } `}
       >
