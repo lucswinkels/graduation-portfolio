865ace8976865db08c527f9d788a808669103b9d
Swinkels, Luc L <l.swinkels-ad@student.fontys.nl>
2024-04-13 02:02:35 +0200

    feat: add copy to clipboard to codeblock and change codeblock style

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Braces, Code, Database, Terminal } from "lucide-react";
+import {
+  Braces,
+  Clipboard,
+  Code,
+  Copy,
+  Database,
+  Terminal,
+} from "lucide-react";
 import Refractor from "react-refractor";
 import bash from "refractor/lang/bash";
 import css from "refractor/lang/css";
@@ -15,6 +22,15 @@ import scss from "refractor/lang/scss";
 import sql from "refractor/lang/sql";
 import tsx from "refractor/lang/tsx";
 import ts from "refractor/lang/typescript";
+
+import { useToast } from "@/components/ui/use-toast";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
 
 const languages = [
   bash,
@@ -43,36 +59,55 @@ export function CodeBlock({
 }: {
   language: string;
   value: string;
-  filename?: string;
+  filename: string;
 }) {
+  const { toast } = useToast();
+  const iconSize = "size-4";
+  //TODO: replace with actual logo's
   const languageIcons: { [key: string]: JSX.Element } = {
-    bash: <Terminal />,
-    css: <Braces />,
-    js: <Code />,
-    json: <Braces />,
-    jsx: <Code />,
-    markup: <Code />,
-    php: <Code />,
-    python: <Code />,
-    sass: <Braces />,
-    scss: <Braces />,
-    sql: <Database />,
-    tsx: <Code />,
-    ts: <Code />,
+    bash: <Terminal className={iconSize} />,
+    css: <Braces className={iconSize} />,
+    js: <Code className={iconSize} />,
+    json: <Braces className={iconSize} />,
+    jsx: <Code className={iconSize} />,
+    markup: <Code className={iconSize} />,
+    php: <Code className={iconSize} />,
+    python: <Code className={iconSize} />,
+    sass: <Braces className={iconSize} />,
+    scss: <Braces className={iconSize} />,
+    sql: <Database className={iconSize} />,
+    tsx: <Code className={iconSize} />,
+    ts: <Code className={iconSize} />,
   };
-
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(value);
+    toast({
+      description: "Copied to clipboard!",
+    });
+  };
   return (
     <>
-      {filename && (
-        <div className="bg-background border-b rounded-tr-lg rounded-tl-lg">
+      <div className="bg-[color:hsl(240,10%,3.9%)] border-[color:hsl(240,3.7%,15.9%)] text-[color:hsl(240,5%,64.9%)] border rounded-tr-lg rounded-tl-lg px-4 py-3 flex justify-between items-center">
+        <div className="flex items-center gap-2">
           {languageIcons[language]}
-          <span>{filename}</span>
+          <span className="text-sm">{filename}</span>
         </div>
-      )}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Copy
+                className="size-4 cursor-pointer hover:text-[color:hsl(0,0%,98%)] transition-colors"
+                onClick={handleCopyToClipboard}
+              />
+            </TooltipTrigger>
+            <TooltipContent>Copy to clipboard</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
       <Refractor
         language={language}
         value={value}
-        className="border bg-background rounded-tl-none rounded-tr-none"
+        className="bg-[color:hsl(240,3.7%,5%)] border-[color:hsl(240,3.7%,15.9%)] text-[color:hsl(240,5%,64.9%)] border border-t-0 rounded-tl-none rounded-tr-none"
       />
     </>
   );
