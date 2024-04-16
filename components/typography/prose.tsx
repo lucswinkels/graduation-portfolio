1ed630e490d1fac8b5713dd1f8269c15ac707027
Luc Swinkels <luc.swinkels@live.nl>
2024-04-16 12:04:16 +0200

    fix: fix prose link styling

--- components/typography/prose.tsx
+++ components/typography/prose.tsx
@@ -12,7 +12,7 @@ export function Prose({
   return (
     <div
       className={cn(
-        "prose prose-img:mt-0 prose-li:text-foreground/80 prose-video:mt-0 prose-p:text-foreground/80 dark:prose-p:text-foreground/95 prose-a:text-foreground marker:text-foreground/40 dark:prose-li:text-foreground/95 dark:prose-invert max-w-full",
+        "prose prose-img:mt-0 prose-video:mt-0 prose-li:text-foreground/70 dark:prose-li:text-foreground/90 prose-p:text-foreground/70 dark:prose-p:text-foreground/90 marker:text-foreground/40 dark:prose-invert max-w-full",
         className
       )}
     >
